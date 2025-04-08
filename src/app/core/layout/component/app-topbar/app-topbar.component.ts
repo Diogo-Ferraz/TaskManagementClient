8c57bfa8b6b3fa5bbd020c38e5695b4fda32139b
Diogo Ferraz <57525336+Diogo-Ferraz@users.noreply.github.com>
2025-04-08 19:55:16 +0100

    feat: add layout service

--- src/app/core/layout/component/app-topbar/app-topbar.component.ts
+++ src/app/core/layout/component/app-topbar/app-topbar.component.ts
@@ -1,6 +1,7 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { StyleClassModule } from 'primeng/styleclass';
+import { LayoutService } from '../../services/layout.service';
 
 @Component({
   selector: 'app-topbar',
@@ -10,5 +11,9 @@ import { StyleClassModule } from 'primeng/styleclass';
   styleUrl: './app-topbar.component.scss'
 })
 export class AppTopbarComponent {
+  constructor(public layoutService: LayoutService) { }
 
+  toggleDarkMode() {
+    this.layoutService.layoutConfig.update((state) => ({ ...state, darkTheme: !state.darkTheme }));
+  }
 }
